fbddd241cdc04ab8522f224736c82d526fcd1c2d
Kirill Kornev <kirill_kornew@mail.ru>
2022-06-04 12:45:53 +0300

    Binary search

---
+++ Search/Binary search/Binary search.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+func binarySearch(_ list: [Int], item: Int) -> Bool {
+    // Borders
+    var leftIndex = 0
+    var rightIndex = list.count - 1
+
+    while leftIndex <= rightIndex {
+        let midIndex = (leftIndex + rightIndex) / 2
+        let midValue = list[midIndex]
+
+
+        if midValue == item {
+            return true
+        } else if midValue > item {
+            // too much - move right border
+            rightIndex = midIndex - 1
+        } else {
+            // too little - move left border
+            leftIndex = midIndex + 1
+        }
+    }
+
+    return false
+}
+
+binarySearch([1, 2, 3, 4, 5], item: 5)
