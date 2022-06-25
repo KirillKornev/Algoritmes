526ce5a84f4c148500d787f83e6d0416dd4c87f8
Kirill Kornev <kirillkornev@Kirills-MacBook-Pro.local>
2022-06-25 13:04:39 +0300

    Added task TwoSum

---
+++ Problems/Array/Two Sum/Two Sum.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+/*
+ Go throught all elemets in array and compare with other all elemets
+ 
+ for
+   for
+ 
+ Compexity O(n^2)
+ */
+
+class Solution {
+    
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        // Пробегаем по всему массиву
+        for i in 0..<nums.count {
+            for j in i + 1..<nums.count {
+                let firstNumber = nums[i]
+                let secondNumber = nums[j]
+                
+                if firstNumber == target - secondNumber {
+                    return [i, j]
+                }
+            }
+        }
+            
+        return []
+    }
+}
+
+_ = Solution().twoSum([1, 2, 3], 6)
