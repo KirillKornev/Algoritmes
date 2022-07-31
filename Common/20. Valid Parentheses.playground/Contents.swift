aa1fb194481e758e1092b16ba8131d680ab4f328
Kirill Kornev <kirill_kornew@mail.ru>
2022-07-31 23:03:57 +0300

    20. Valid Parentheses task

---
+++ Common/20. Valid Parentheses.playground/Contents.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+/*
+ Idea:
+ make push to stack with open bracekts
+ make pop to stack with close brackets
+
+ if stack is not empty at the end - false
+
+ _____________________
+ Example1 ()[]{}
+
+ push ( | [(]
+ pop  ) | []
+
+ push [ | [[]
+ pop  ] | []
+
+ push { | [{]
+ pop  } | []
+
+ _____________________
+ Example 2 ")()"
+
+ push ) | [)]
+ push ( | [)(]
+ pop  ) | [)]
+ */
+
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        guard s.count > 1 else { return false }
+        var stack: [Character] = []
+
+        var bracketsDict: [Character: Character] = [
+            "]": "[",
+            ")" : "(",
+            "}": "{"
+        ]
+
+        for character in s {
+            // isOpen
+            if bracketsDict.values.contains(character) {
+                // make push
+                stack.append(character)
+                continue
+            }
+
+            // isClose
+            if bracketsDict.keys.contains(character), let peek = stack.last, peek == bracketsDict[character] {
+                // make pop
+                stack.removeLast()
+            } else {
+                // first close bracket
+                stack.append(character)
+            }
+        }
+
+        return stack.isEmpty
+    }
+}
+
+Solution().isValid(")()")
