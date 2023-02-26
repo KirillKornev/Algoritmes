3b5f0ce5b3cdea8d829a733fc53625c11f8d349a
Kirill Kornev <kirill_kornew@mail.ru>
2023-02-26 19:17:01 +0400

    1143. Longest Common Subsequence

---
+++ 📦 Services/LeetCode/Dynamic/1143. Longest Common Subsequence.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// https://leetcode.com/problems/longest-common-subsequence/description/
+
+class Solution {
+    func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
+        var array1 = Array(text1)
+        var array2 = Array(text2)
+
+        var dp = Array(repeating: Array(repeating: 0, count: text2.count + 1), count: text1.count + 1)
+
+        for i in 1...array1.count {
+            for j in 1...array2.count {
+                if array1[i - 1] == array2[j - 1] {
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                }
+            }
+        }
+
+        return dp[text1.count][text2.count]
+    }
+}
+
+Solution().longestCommonSubsequence("abc", "def")
