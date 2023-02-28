4fa61d1db1e2bf9f62727f548be43eceded6d9d5
Kirill Kornev <kirill_kornew@mail.ru>
2023-02-28 21:21:49 +0400

    Task 198

--- 📦 Services/LeetCode/Dynamic/198. House Robber.playground/Contents.swift
+++ 📦 Services/LeetCode/Dynamic/198. House Robber.playground/Contents.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+// https://leetcode.com/problems/house-robber/
+// https://leetcode.com/problems/house-robber-ii/
+
+// First task - find best solution for each home
+// Second task - find solution for 2 subarrays [0...i-1] and [1...i], because last homes is connected
+
 class Solution {
     func rob(_ nums: [Int]) -> Int {
         if nums.count == 0 { return 0 }
@@ -15,6 +21,16 @@ class Solution {
 
         return dp[nums.count - 1]
     }
+
+    func robII(_ nums: [Int]) -> Int {
+        if nums.count == 0 { return 0 }
+        if nums.count == 1 { return nums[0] }
+
+        let firstSubArray = Array(nums[1...nums.count - 1])
+        let lastSubArray = Array(nums[0...nums.count - 2])
+
+        return max(rob(firstSubArray), rob(lastSubArray))
+    }
 }
 
-Solution().rob([2,7,9,3,1])
+Solution().robII([2,3,2])
